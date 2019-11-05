4f6a83ef91c1dfc6207bab6e1ca98e464c5d4c0f
Gopi <gopi@gmail.com>
2019-11-05 11:40:28 +0530

    my first shell program'

---
+++ shell.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+mkdir gopi
+echo "gopi folder created successfully"
+touch gopi/login{1..5}.aspx
+echo "Login pages are created"
+ls -a
+echo "Displaying all files"
+
+
+#system Defined variables
+echo $BASH
+echo $PWD
+
+# user defined variables
+
+name=gopi
+lastname=$name.nath
+
+echo $lastname
+
+
+
+
